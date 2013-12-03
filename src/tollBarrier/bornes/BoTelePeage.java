81f9d315fe42a7e7c8c89a972a695d7fe6441909
Seichais Robin <robin.seichais@ecole.ensicaen.fr>
2013-12-03 12:55:20 +0100

    Definition protos et tests bornes

---
+++ src/tollBarrier/bornes/BoTelePeage.java
@@ -0,0 +1,27 @@
+package tollBarrier.bornes;
+
+public class BoTelePeage extends Borne{
+
+	public BoTelePeage(int numBorne){
+
+		super(numBorne);
+		_payment = {Telepeage};		
+	}
+	
+	public void leverBarriere(){
+		
+	}
+	
+	public void envoyerRapport(){
+		
+	}
+	
+	public boolean demanderAccord(){
+		
+	}
+	
+	public void envoyerAlarme(){
+		
+	}
+	
+}
