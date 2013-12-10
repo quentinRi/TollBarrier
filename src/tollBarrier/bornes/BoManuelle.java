6bc3ffa27fa40248a7375c93e2946c906579eb23
Seichais Robin <robin.seichais@ecole.ensicaen.fr>
2013-12-10 12:30:48 +0100

    Ajout FabriqueDeBorne et des Tests Methode leverBarriere Suppression attribut numBorne.

--- src/tollBarrier/bornes/BoManuelle.java
+++ src/tollBarrier/bornes/BoManuelle.java
@@ -1,27 +1,37 @@
 package tollBarrier.bornes;
+import java.util.ArrayList;
+import tollBarrier.vehicule.MoyenDePaiment;
+
+import java.util.ArrayList;
+
+import tollBarrier.vehicule.MoyenDePaiment;
 
 public class BoManuelle extends Borne{
 
-	public BoManuelle(int numBorne){
+	public BoManuelle(){
 
-		super(numBorne);
-		_payment = {CB, Liquide, Abonnement};
+		super();
+		_payment = new ArrayList<MoyenDePaiment>();
+		_payment.add(MoyenDePaiment.CB);
+		_payment.add(MoyenDePaiment.Liquide);
+		_payment.add(MoyenDePaiment.Abonnement);
 	}
 	
 	public void leverBarriere(){
 		
+		_paymentAccepte = demanderAccord();
+		if(_paymentAccepte)
+			_barriereLevee = true;
 	}
-	
+
+/*
 	public void envoyerRapport(){
 		
+		
 	}
-	
+*/
 	public boolean demanderAccord(){
-		
-	}
-	
-	public void envoyerAlarme(){
-		
+		return false;
 	}
 	
 }
--- src/tollBarrier/bornes/BoTelePeage.java
+++ src/tollBarrier/bornes/BoTelePeage.java
@@ -1,27 +1,31 @@
 package tollBarrier.bornes;
+import java.util.ArrayList;
+import tollBarrier.vehicule.MoyenDePaiment;
 
 public class BoTelePeage extends Borne{
 
-	public BoTelePeage(int numBorne){
+	public BoTelePeage(){
 
-		super(numBorne);
-		_payment = {Telepeage};		
+		super();	
+		_payment = new ArrayList<MoyenDePaiment>();
+		_payment.add(MoyenDePaiment.Telepeage);	
 	}
 	
 	public void leverBarriere(){
 		
+		_paymentAccepte = demanderAccord();
+		if(_paymentAccepte)
+			_barriereLevee = true;
 	}
-	
+/*
 	public void envoyerRapport(){
 		
 	}
+*/
 	
 	public boolean demanderAccord(){
-		
+		return false;
 	}
-	
-	public void envoyerAlarme(){
-		
-	}
-	
+
+
 }
