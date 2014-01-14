eaed2cf6e2d105489efd9b93fe49d358b7163535
Seichais Robin <robin.seichais@ecole.ensicaen.fr>
2014-01-14 12:21:24 +0100

    Modification acceleration: 10x standard

--- src/tollBarrier/bornes/Borne.java
+++ src/tollBarrier/bornes/Borne.java
@@ -31,7 +31,7 @@ public abstract class Borne extends Thread
 	private long _time;
 	private int num;
 	private static int nbInstance = 0;
-	private int _acc;
+	private boolean _accFlag;
 	private int _alarmeRate;
 	private int _nbAlarme;
 
@@ -41,7 +41,7 @@ public abstract class Borne extends Thread
 		nbInstance++;
 		_vehicule = null;
 		_nbVeh = 0;
-		_acc = 1;
+		_accFlag = false;
 		_alarmeRate = 1000;
 		_nbAlarme = 0;
 	}
@@ -71,15 +71,9 @@ public abstract class Borne extends Thread
 		return tempsPassage;
 	}
 	
-	public void accelerate(int n){
+	public void accelerate(){
 		
-		if(n==0){
-			System.err.println("Erreur: acceleration par 0.");
-			_acc = 1;
-		}
-		else{
-			_acc = n;
-		}
+		_accFlag = !_accFlag;
 	}
 
 	public boolean demanderAccord()
@@ -103,7 +97,7 @@ public abstract class Borne extends Thread
 		if(n == 1) time += 108000;
 		try
 		{
-			time = time/_acc;
+			if(_accFlag) time = time/10;
 			Thread.sleep(time);
 		} catch (InterruptedException e)
 		{
@@ -123,7 +117,9 @@ public abstract class Borne extends Thread
 			{
 				try
 				{
-					Thread.sleep(n/_acc);
+					if(_accFlag) n = 10;
+					else n = 100;
+					Thread.sleep(n);
 				} catch (InterruptedException e)
 				{
 					e.printStackTrace();
@@ -162,7 +158,7 @@ public abstract class Borne extends Thread
 				* mdp.getTimeMultiplier() + additionalTime();
 		try
 		{
-			time = time/_acc;
+			if(_accFlag) time = time/10;
 			Thread.sleep(time);
 		} catch (InterruptedException e)
 		{ 
